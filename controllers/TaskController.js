cf6b50867395d9f72e73ec65c29a8a66ea04f528
AranBeitia <arantza.beitia@gmail.com>
2023-06-14 13:59:19 +0200

    Feat: create delete endpoint

--- controllers/TaskController.js
+++ controllers/TaskController.js
@@ -10,6 +10,35 @@ const TaskController = {
 			res.status(500).send({ message: 'error', error })
 		}
 	},
+	async getAll(req, res) {
+		try {
+			const tasks = await Task.find()
+			res.status(200).send({ message: 'todo ok', tasks })
+		} catch (error) {
+			res
+				.status(500)
+				.send({ message: 'Ha habido un problema al recuperar tareas' })
+		}
+	},
+	async getById(req, res) {
+		try {
+			const task = await Task.findById(req.params._id)
+			res.status(200).send(task)
+		} catch (error) {
+			res.status(500).send({ message: 'Error al recuperar tarea por id' })
+		}
+	},
+	async delete(req, res) {
+		try {
+			const task = await Task.findByIdAndDelete(req.params._id)
+			res.send({ task, message: 'Product deleted' })
+		} catch (error) {
+			console.error(error)
+			res.status(500).send({
+				message: 'there was a problem trying to remove the publication',
+			})
+		}
+	},
 }
 
 module.exports = TaskController
--- routes/tasks.js
+++ routes/tasks.js
@@ -2,6 +2,11 @@ const express = require('express')
 const router = express.Router()
 const TaskController = require('../controllers/TaskController')
 
-router.post('/', TaskController.create)
+router.post('/create', TaskController.create)
+router.get('/', TaskController.getAll)
+router.get('/id/:_id', TaskController.getById)
+// router.put('/id/:_id', TaskController.complete)
+// router.put('/id/:_id', TaskController.update)
+router.delete('/id/:_id', TaskController.delete)
 
 module.exports = router
